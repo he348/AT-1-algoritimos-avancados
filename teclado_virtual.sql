dd055683b730aaf013e560301912f5f441c9f4f3
João Pedro Papp <joao_pp02@hotmail.com>
2024-03-25 08:44:32 -0300

    Add files via upload
    Código SQL utilizado para a criação do banco e sincronização com o Flask

---
+++ teclado_virtual.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 25/03/2024 às 12:38
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `teclado_virtual`
+--
+CREATE DATABASE IF NOT EXISTS `teclado_virtual` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `teclado_virtual`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sessoes`
+--
+
+DROP TABLE IF EXISTS `sessoes`;
+CREATE TABLE `sessoes` (
+  `id` int(11) NOT NULL,
+  `id_sessao` varchar(128) NOT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- RELACIONAMENTOS PARA TABELAS `sessoes`:
+--
+
+--
+-- Despejando dados para a tabela `sessoes`
+--
+
+INSERT INTO `sessoes` (`id`, `id_sessao`, `data`) VALUES
+(1, '6D4DqU1v-i6NKLpP1tC-UA', '2024-03-23 13:35:15'),
+(2, 'mfFB1sXDp2TJ1_jBVy1ocQ', '2024-03-23 13:57:59');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `senha_hash` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- RELACIONAMENTOS PARA TABELAS `usuarios`:
+--
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `senha_hash`) VALUES
+(1, 'admin', '*A4B6157319038724E3560894F7F932C8886EBFCF');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `sessoes`
+--
+ALTER TABLE `sessoes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `sessoes`
+--
+ALTER TABLE `sessoes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
